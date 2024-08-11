18088b33d77c2aa0a0220dfe5637720b42743c9b
Julio Anccasi <julio.anccasi.dev@gmail.com>
2024-08-11 00:06:12 -0500

    Show Issues

--- src/issues/actions/get-label.actions.ts
+++ src/issues/actions/get-label.actions.ts
@@ -6,9 +6,9 @@ export const getLabels = async(): Promise<GithubLabel[]> => {
 
   await sleep(1500);
 
-  const { data } = await githubApi.get<GithubLabel[]>('labels');
+  const { data } = await githubApi.get<GithubLabel[]>('/labels');
   
-  console.log(data);
+  // console.log(data);
   
   return data;
 }
