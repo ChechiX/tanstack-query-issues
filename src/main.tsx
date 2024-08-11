747de32f13bce36ca29c16b524eb82dec5c946d8
Julio Anccasi <julio.anccasi.dev@gmail.com>
2024-08-11 10:04:32 -0500

    Load Issue Comments

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './index.css';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
