530e34958d17d260320346765b0bcf79a9a83a7f
Julio Anccasi <julio.anccasi.dev@gmail.com>
2024-08-12 11:10:32 -0500

    Filter Issues - Label

--- src/issues/actions/get-issues.action.ts
+++ src/issues/actions/get-issues.action.ts
@@ -2,7 +2,7 @@ import { githubApi } from "../../api/github.api";
 import { sleep } from "../../helpers";
 import { GithubIssue, State } from "../interfaces";
 
-export const getIssues = async( state: State ): Promise<GithubIssue[]> => {
+export const getIssues = async( state: State, selectedLabels: string[]): Promise<GithubIssue[]> => {
   
   await sleep(1500);
 
@@ -10,6 +10,10 @@ export const getIssues = async( state: State ): Promise<GithubIssue[]> => {
 
   if ( state !== State.All ) {
     params.append('state', state );
+  }
+
+  if ( selectedLabels.length > 0 ) {
+    params.append('labels', selectedLabels.join(','))
   }
 
   const { data } = await githubApi.get<GithubIssue[]>('/issues', {
--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { GithubIssue, State } from '../interfaces';
 import { useQueryClient } from '@tanstack/react-query';
 import { getIssue, getIssueComments } from '../actions';
+import { timeSince } from '../../helpers';
 
 interface Props {
   issue: GithubIssue;
@@ -57,9 +58,25 @@ export const IssueItem = ({ issue }: Props ) => {
         </a>
         <span className="text-gray-500">
           {/* TODO: days ago */}
-          #${ issue.number } opened 2 days ago by{' '}
+          #${ issue.number } opened { timeSince( issue.created_at )} ago by{' '}
           <span className="font-bold">{ issue.user.login }</span>
         </span>
+
+        <div className="flex flex-wrap">
+          {
+            issue.labels.map( label => (
+              <span 
+                key={ label.id }
+                className="px-2 mr-2 py-2 text-xs text-white rounded-md"
+                style={{
+                  border: `1px solid #${ label.color }`
+                }}
+              >
+                { label.name }
+              </span>
+            ))
+          }
+        </div>
       </div>
 
       <img
